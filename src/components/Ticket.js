6b606bf0013e6d3efa2c86b999186e46d69aafc2
Brian Scherner <brianscherner@gmail.com>
2024-03-07 15:06:02 -0800

    Add propTypes to Ticket component and make them required.

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Ticket(props) {
   return (
@@ -8,6 +9,12 @@ function Ticket(props) {
       <hr/>
     </React.Fragment>
   );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string.isRequired
 }
 
 export default Ticket;
