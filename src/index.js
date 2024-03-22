442d8e8232302e053706a467b224d2283332aa7c
Brian Scherner <brianscherner@gmail.com>
2024-03-22 11:58:16 -0700

    Add Redux to root file.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './components/index.css';
 import App from './components/App';
+import { configureStore } from 'redux';
+import reducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
+const store = configureStore(reducer);
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>
 );
