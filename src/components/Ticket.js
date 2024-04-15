e119a5b3c5b7572fab4f6836e1717f9ce0bda32d
Brian Scherner <brianscherner@gmail.com>
2024-04-15 11:44:25 -0700

    Add date-fns package and functionality to create timestamps and sort by creation date in UI.

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -7,6 +7,7 @@ function Ticket(props) {
       <div onClick = {() => props.whenTicketClicked(props.id)}>
         <h3>{props.location} - {props.names}</h3>
         <p><em>{props.issue}</em></p>
+        <p><em>{props.formattedWaitTime}</em></p>
         <hr/>
       </div>
     </React.Fragment>
@@ -17,6 +18,7 @@ Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string.isRequired,
+  formattedWaitTime: PropTypes.string.isRequired,
   id: PropTypes.string,
   whenTicketClicked: PropTypes.func
 }
