731c2f158839f82be14498ce3267ecf6970810bf
Brian Scherner <brianscherner@gmail.com>
2024-04-25 15:52:24 -0700

    Add ToggleTheme component and import to App.

---
+++ src/components/ToggleTheme.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ToggleTheme() {
+  return (
+    <React.Fragment>
+      <button>Toggle Theme</button>
+    </React.Fragment>
+  );
+}
+
+export default ToggleTheme;
