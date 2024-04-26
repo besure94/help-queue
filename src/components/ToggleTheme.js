9177364aec1e460bf316a6c82fc0219a9a1e8ff2
Brian Scherner <brianscherner@gmail.com>
2024-04-26 10:25:38 -0700

    Add functionality to toggle color theme for body of application.

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,11 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function ToggleTheme() {
+function ToggleTheme(props) {
+  const { toggleTheme } = props;
+
   return (
     <React.Fragment>
-      <button>Toggle Theme</button>
+      <button onClick={toggleTheme}>Toggle Theme</button>
     </React.Fragment>
   );
+}
+
+ToggleTheme.propTypes = {
+  toggleTheme: PropTypes.func
 }
 
 export default ToggleTheme;
