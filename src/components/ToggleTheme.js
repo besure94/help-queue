9df80ea0c7df4bea3b4c223fd106320d80a38530
Brian Scherner <brianscherner@gmail.com>
2024-04-26 10:52:53 -0700

    Add functionality to change button text depending on which theme is toggled.

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -2,17 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ToggleTheme(props) {
-  const { toggleTheme } = props;
+  const { theme, toggleTheme } = props;
+
+  const styles = {
+    backgroundColor: theme.buttonBackground,
+    color: theme.textColor
+  }
 
   return (
     <React.Fragment>
-      <button onClick={toggleTheme}>Toggle Theme</button>
+      <button style={styles} onClick={toggleTheme}>{theme.textColor === "AntiqueWhite" ? "Toggle Light Theme" : "Toggle Dark Theme"}</button>
     </React.Fragment>
   );
 }
 
 ToggleTheme.propTypes = {
-  toggleTheme: PropTypes.func
+  toggleTheme: PropTypes.func,
+  theme: PropTypes.object
 }
 
 export default ToggleTheme;
